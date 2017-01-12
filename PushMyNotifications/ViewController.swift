6a203c180b78e9e3ea7cbd1c322f432a11e04b59
gayanj <gayan@bluedrop.com>
2017-01-12 08:19:27 -0330

    Initial Commit

---
+++ PushMyNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushMyNotifications
+//
+//  Created by Gayan Jayasundara on 2017-01-12.
+//  Copyright © 2017 Gayan Jayasundara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
